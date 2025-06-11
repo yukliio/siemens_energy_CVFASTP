066a3d3cf24bb5b80b8fba829525c3eab29306d5
yukliio <huangqianyu0722@gmail.com>
2025-06-11 15:34:16 -0400

    init commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# import sys
+# sys.path.append('./modules/')   
+# import modules.utils as utils
+
+
+# utils.rename_files_from_dir("D:/data_folder/460B4496210_PS1_NUTHEXHEAVY")
+# utils.rename_files_from_dir("D:/data_folder/401720107_PS1_RETAININGRING")
---
+++ modules/utils.py
@@ -0,0 +1,18 @@
+# utility helper functions for general use !
+
+import os
+import re
+
+def rename_files_from_dir(folder_path): 
+    """
+    Purpose: renaming files for folder_path directory. 
+    Returns: none. Just changes the file path to image number + item type.
+    """
+    item_type = os.path.basename(os.path.normpath(folder_path))
+    print(item_type)
+    for i, file_name in enumerate(os.listdir(folder_path)): 
+        old_path = os.path.join(folder_path, file_name)
+        print(old_path)
+        new_path = os.path.join(folder_path, f"{i}_{item_type}")
+        os.rename(old_path, new_path)
+        print(f"Renamed: {old_path} --> {new_path}")
