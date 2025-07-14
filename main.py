807855229914557d411ab341465b5e49728bb5e7
yukliio <huangqianyu0722@gmail.com>
2025-07-14 14:17:16 -0400

    added part file handling/cleaning

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 import sys
 sys.path.append('./modules/')   
 import modules.utils as utils
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import PlainTextResponse
 
 
-utils.rename_files_from_dir("D:/data_folder/PW237T093001_PD2_WASHERLOCK")
+app = FastAPI()
+
+@app.get("/")
+async def test(): 
+    return "hello world"
+
+@app.post("/extract_styles_names",)
+async def extract_styles(
+    part_file: UploadFile = File(...),
+    desc_file: UploadFile = File(...), 
+    response_class=PlainTextResponse
+):
+    
+    parts_txt = await part_file.read()
+    desc_parts_txt = await desc_file.read()
+
+    file_name_new = utils.name_parts(parts_txt, desc_parts_txt)
+    for part in file_name_new: 
+        print(part)
+
+    return 0
+
+    # parts = line.strip for line in content.decode("utf-8").splitlines() if line.strip()
+    # print(parts)
+    # part_file_data = await part_file.read()
+    # desc_file_data = await part_file.read()
+    # return utils.name_parts(part_file_data, desc_file_data)
+    
+
 
--- modules/utils.py
+++ modules/utils.py
@@ -2,6 +2,8 @@
 
 import os
 import re
+import glob
+import re
 
 def rename_files_from_dir(folder_path): 
     """
@@ -16,3 +18,43 @@ def rename_files_from_dir(folder_path):
         new_path = os.path.join(folder_path, f"{i}_{item_type}.png")
         os.rename(old_path, new_path)
         print(f"Renamed: {old_path} --> {new_path}")
+
+def deleteDupes(folder_path): 
+    png_files = glob.glob(os.path.join((folder_path), '*png'))
+    for i in range(1, 6): 
+        for file in png_files: 
+            if file.endswith(f"({i}).png"): 
+                os.remove(file)
+                print("deleted")
+
+
+def name_parts(parts_txt, desc_parts_txt): 
+    parts = []
+    descs = []
+    file_name_new = []
+
+    import re
+
+    for part in parts_txt.decode("utf-8").splitlines(): 
+        part_cleaned= re.sub(r"[\s,]+", "", part)
+        parts.append(part_cleaned)
+    for desc in desc_parts_txt.decode("utf-8").splitlines(): 
+        desc_cleaned= re.sub(r"[\s,]+", "", desc)
+        descs.append(desc_cleaned)
+
+    for desc, part in zip(descs, parts):
+        new_file = f"{part}_{desc}"
+        file_name_new.append(new_file)
+    
+    return file_name_new
+    
+    
+
+    
+
+
+
+
+    
+    
+
